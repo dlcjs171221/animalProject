f558b84349935b31daecc3b6fd5b331c298ecb75
koitt04a <koitt04a@DESKTOP-RHVIVA1>
2020-03-20 14:52:49 +0900

    map 업데이트

--- 20.03.19_STS_animal/src/main/java/com/pub/animal/MapAction.java
+++ 20.03.19_STS_animal/src/main/java/com/pub/animal/MapAction.java
@@ -2,22 +2,31 @@ package com.pub.animal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 public class MapAction {
-
+	
+	
+	
 	@RequestMapping("/map.inc")
-	public ModelAndView map(String careAddr,String nowPage) {
-		
-		System.out.println(careAddr);
+	public ModelAndView map(String careAddr,String nowPage, String desertionNo) {
+
 		ModelAndView mv = new ModelAndView();
 		
-		mv.addObject("careAddr",careAddr);
-		mv.addObject("nowPage",nowPage);
+
+		mv.addObject("careAddr", careAddr);
+		mv.addObject("nowPage", nowPage);
+		mv.addObject("desertionNo", desertionNo);
 		
 		mv.setViewName("map");
 		
+		
 		return mv;
 	}
+	
+	
 }
